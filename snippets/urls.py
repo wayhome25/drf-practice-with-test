7e2c852c94f5c9caad92e4f2ef280ebf2c3fa52e
June <wayhome250@gmail.com>
2018-09-02 12:35:23 +0900

    APIView 사용하도록 수정

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    url(r'^$', views.snippet_list, name='snippet_list'),
-    url(r'^(?P<pk>\d+)/$', views.snippet_detail, name='snippet_detail'),
+    url(r'^$', views.SnippetList.as_view(), name='snippet_list'),
+    url(r'^(?P<pk>\d+)/$', views.SnippetDetail.as_view(), name='snippet_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -1,25 +1,25 @@
 from django.http.response import JsonResponse, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-@api_view(['GET', 'POST'])
-def snippet_list(request, format=None):
+class SnippetList(APIView):
     """List all code snippets, or create a new snippet"""
 
-    if request.method == 'GET':
-        snippet = Snippet.objects.all()
-        serializer = SnippetSerializer(snippet, many=True)
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(instance=snippets, many=True)
         return Response(data=serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -27,26 +27,26 @@ def snippet_list(request, format=None):
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk, format=None):
+class SnippetDetail(APIView):
     """Retrieve, update or delete a code snippet."""
 
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        return get_object_or_404(Snippet, pk=pk)
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(instance=snippet)
         return Response(data=serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = SnippetSerializer(instance=snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data)
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serialzier = SnippetSerializer(instance=snippet, data=request.data)
+        if serialzier.is_valid():
+            serialzier.save()
+            return Response(data=serialzier.data)
+        return Response(serialzier.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
