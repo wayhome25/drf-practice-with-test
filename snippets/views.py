9430884a2bcf1cf5ceb0a7cfecd6add1389a129f
June <wayhome250@gmail.com>
2018-09-04 11:07:27 +0900

    edit views using generic class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,34 +1,17 @@
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import (ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin,
-                                   DestroyModelMixin)
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(ListModelMixin, CreateModelMixin, GenericAPIView):
-    """List all code snippets, or create a new snippet"""
+class SnippetList(ListCreateAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView):
-    """Retrieve, update or delete a code snippet."""
+class SnippetDetail(RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
