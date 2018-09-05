67de41569496c7a6ff8eba42894b102c349173ab
June <wayhome250@gmail.com>
2018-09-05 19:33:27 +0900

    git hook test

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 
+from quickstart import views
 
 urlpatterns = [
     url(r'^', include('snippets.urls')),
